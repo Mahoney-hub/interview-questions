0766608379d7ad06be901035313968b3b7bb7304
Pavel Vasilev <pv9998286620@yandex.ru>
2022-03-14 17:11:15 +0300

    added Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Row, Col, Button, Divider, Card} from 'antd';
+import {Layout, Menu, Breadcrumb, Row, Col, Button, Divider, Card} from 'antd';
 import {questions} from './questions';
 
 type RenderQuestionType = {
@@ -9,6 +9,7 @@ type RenderQuestionType = {
 }
 
 const App = () => {
+    const {Header, Content, Footer} = Layout;
 
     const renderQuestion = () => {
         const min = 0;
@@ -30,22 +31,37 @@ const App = () => {
 
     return (
         <div>
-            <Divider orientation="left">Quiz</Divider>
-            <Row justify={'center'}>
-                <Col span={10}>
-                    <Card
-                        hoverable
-                        title={question.question}
-                        bordered={false}
-                    >
-                        <p style={{display: text, fontSize: '20px'}}>{question.answer}</p>
-                        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <Button type="primary" onClick={showP}>Показать ответ</Button>
-                            <Button type="primary" danger onClick={nextQuestion}>Следующий вопрос</Button>
-                        </div>
-                    </Card>
-                </Col>
-            </Row>
+            <Layout>
+                <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content className="site-layout" style={{padding: '0 50px', marginTop: 64, height: '85vh'}}>
+                    <Row justify={'center'} style={{marginTop: 30}}>
+                        <Col span={10}>
+                            <Card
+                                hoverable
+                                title={question.question}
+                                bordered={false}
+                            >
+                                <p style={{display: text, fontSize: '20px'}}>{question.answer}</p>
+                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                    <Button type="primary" onClick={showP}>Показать ответ</Button>
+                                    <Button type="primary" danger onClick={nextQuestion}>Следующий вопрос</Button>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    Проект выполнил Павел Васильев для подготовки к
+                    трудоустройству
+                </Footer>
+            </Layout>
         </div>
     );
 };
